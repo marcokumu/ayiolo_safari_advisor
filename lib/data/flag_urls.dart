5bf2b3179f8c507c05d55e534217cf095e85e8d1
marcokumu <marcokumu10@gmail.com>
2024-01-16 13:37:45 +0300

    📄data: added urls of the rquired country flags and an array of the country names

---
+++ lib/data/flag_urls.dart
@@ -0,0 +1,17 @@
+final List<String> flagURLS = [
+  'https://imgs.search.brave.com/xcLGNF8IySVJLBDWrSnCEo93tZizJ0DvQwitnktf7To/rs:fit:200:200:1/g:ce/aHR0cHM6Ly9jZG4u/YnJpdGFubmljYS5j/b20vMTUvMTUtMDA0/LUI1RDZCRjgwL0Zs/YWctS2VueWEuanBn',
+  'https://imgs.search.brave.com/_RtzNutJ3u7YYAmfIBAeQ23_-qZ9o7FzCHpGJkmB3rM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzAxLzIxLzU1/LzM2MF9GXzEyMTU1/MDBfWWpUVVZXMmxt/QUJoNHdLTE1zRmxl/ZlhZNG9CaGk1Lmpw/Zw',
+  'https://imgs.search.brave.com/47tRn2D4CvGW2LtDCDIDNeglBdo1YwL3OHbinCPZ3Vs/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAxLzA3LzU1LzQw/LzM2MF9GXzEwNzU1/NDAwMl9HRkg0d0Np/V2RtZXBIaERyT1dy/Z1ZxeVFNMG51MFpD/Ni5qcGc',
+  'https://imgs.search.brave.com/125eV1xv3_unPG6SagKuJoJG8RbymvqkMJbfsqQt3_Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAxLzA0LzQ3LzE3/LzM2MF9GXzEwNDQ3/MTczMV9Ham5JWWc3/MllGWXVyNFBuN1dt/OENjRlpjNHJzeWV0/NC5qcGc',
+  'https://imgs.search.brave.com/ZiQfI_kBxczPULMLdxtMYi1xvph05m1DLEXwL6CHh2I/rs:fit:200:200:1/g:ce/aHR0cHM6Ly9jZG4u/YnJpdGFubmljYS5j/b20vMjAvMjAtMDA0/LUVCMjBBMDI2L0Zs/YWctVGFuemFuaWEu/anBn',
+  'https://imgs.search.brave.com/kCnNbT3aiH3gHgi7UYEXtJmNtIlcBDsQpJMDoAv15Fs/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/YnJpdGFubmljYS5j/b20vMjIvMjItMDA0/LTAxNjU5NzVEL0Zs/YWctVWdhbmRhLmpw/Zw',
+];
+
+final List<String> countryNames = [
+  'Kenya',
+  'South Africa-Coming Soon',
+  'Zambia-Coming Soon',
+  'Botswana-Coming Soon',
+  'Tanzania-Coming Soon',
+  'Uganda-Coming Soon',
+];
