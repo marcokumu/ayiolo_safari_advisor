a92528af644ab872f22dc1f7e5f9bb4b915b5f97
marcokumu <marcokumu10@gmail.com>
2024-01-16 14:23:03 +0300

    ✨feat: used GridView to create Containers for images

---
+++ lib/screens/locations.dart
@@ -0,0 +1,62 @@
+import 'package:ayiolo_safari_advisor/data/flag_urls.dart'; // Assuming flagURLS and countryNames are defined here
+import 'package:flutter/material.dart';
+
+class LocationNavItem extends StatelessWidget {
+  const LocationNavItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Locations'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemCount: flagURLS.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              if (index == 0) {
+                Navigator.pushNamed(context, '/parks');
+              }
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Image.network(
+                      flagURLS[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Container(
+                    color:
+                        const Color.fromARGB(255, 71, 70, 70).withOpacity(0.4),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      countryNames[index],
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/parks.dart
@@ -0,0 +1,61 @@
+import 'package:ayiolo_safari_advisor/data/park_image.dart';
+import 'package:flutter/material.dart';
+
+class Parks extends StatelessWidget {
+  const Parks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Parks'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemCount: parkImages.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              if (index == 0) {
+                Navigator.pushNamed(context, '');
+              }
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Image.asset(
+                      parkImages[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Container(
+                    color: Colors.black.withOpacity(0.7),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      parkNames[index],
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
