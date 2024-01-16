84e5924a23d8047922fb8269098dc2bc0476ae9e
marcokumu <marcokumu10@gmail.com>
2024-01-16 13:40:36 +0300

    📄data: added an array of the required park names in Kenya.
    This file also includes an array of relative paths of the required park images

---
+++ lib/data/park_image.dart
@@ -0,0 +1,17 @@
+final List<String> parkImages = [
+  'assets/images/0.jpg',
+  'assets/images/1.jpg',
+  'assets/images/2.jpg',
+  'assets/images/3.jpg',
+  'assets/images/4.jpg',
+  'assets/images/5.jpg',
+];
+
+final List<String> parkNames = [
+  'Nairobi National Park',
+  'Amboseli National Park - Coming Soon',
+  'Maasai Mara National Reserve - Coming Soon',
+  'Lake Nakuru National Park - Coming Soon',
+  'Tsavo East National Park - Coming Soon',
+  'Tsavo West National Park - Coming Soon',
+];
