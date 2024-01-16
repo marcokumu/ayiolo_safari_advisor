af82c43f24f29be2f5be96f46ca95aea1d2658f6
marcokumu <marcokumu10@gmail.com>
2024-01-16 13:50:36 +0300

    ✨feat: created a scrollable list of all the wild animals .
    Added a divider after every animal on the list.

---
+++ lib/widgets/animal_list.dart
@@ -0,0 +1,43 @@
+import 'package:ayiolo_safari_advisor/data/wild_animals.dart';
+import 'package:flutter/material.dart';
+
+class AnimalList extends StatefulWidget {
+  final Function(String) onAnimalSelected;
+
+  const AnimalList({super.key, required this.onAnimalSelected});
+
+  @override
+  AnimalListState createState() => AnimalListState();
+}
+
+class AnimalListState extends State<AnimalList> {
+  List<String> selectedAnimals = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: wildAnimals.length,
+      itemBuilder: (context, index) {
+        final animal = wildAnimals[index];
+        return Column(
+          children: [
+            Center(
+              child: ListTile(
+                title: Text(animal),
+                onTap: () {
+                  widget.onAnimalSelected(animal);
+                },
+              ),
+            ),
+            const Divider(
+              color: Colors.black,
+              thickness: 0.3,
+              indent: 6.0,
+              endIndent: 6.0,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
