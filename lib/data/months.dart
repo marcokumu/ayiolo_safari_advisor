ceb5b0c76556e999734b23b84988ef5c16f1b772
marcokumu <marcokumu10@gmail.com>
2024-01-16 13:35:19 +0300

    📄data: added arrays of months in different years

---
+++ lib/data/months.dart
@@ -0,0 +1,44 @@
+final List<String> year1 = [
+  'January 2023',
+  'February 2023',
+  'March 2023',
+  'April 2023',
+  'May 2023',
+  'June 2023',
+  'July 2023',
+  'August 2023',
+  'September 2023',
+  'October 2023',
+  'November 2023',
+  'December 2023',
+];
+
+final List<String> year2 = [
+  'January 2024',
+  'February 2024',
+  'March 2024',
+  'April 2024',
+  'May 2024',
+  'June 2024',
+  'July 2024',
+  'August 2024',
+  'September 2024',
+  'October 2024',
+  'November 2024',
+  'December 2024',
+];
+
+final List<String> year3 = [
+  'January 2025',
+  'February 2025',
+  'March 2025',
+  'April 2025',
+  'May 2025',
+  'June 2025',
+  'July 2025',
+  'August 2025',
+  'September 2025',
+  'October 2025',
+  'November 2025',
+  'December 2025',
+];
